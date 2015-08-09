b16f7b9013eef0eaa7787be6a2042668642989b5
JKasyan <kasyan86@i.ua>
2015-08-09 23:40:23 +0300

    First commit(Added files)

---
+++ src/main/resources/static/js/services.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Evgen on 09.08.2015.
+ */
+
+angular.module('catalogApp',[]).
+    factory('booksService',['$http',function($http){
+        return {
+            getBooks: function(){
+                return $http.get('getBooks');
+            }
+        }
+    }]).factory('authorsService',['$http',function($http){
+        return {
+            getAuthors: function(){
+                return $http.get('getAuthors');
+            }
+        }
+    }]);
