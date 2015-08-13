2b5458aca633a25292f12cdc02d88ecb78848882
evgen <kasyan86@i.ua>
2015-08-13 17:56:47 +0300

    Added dao layer

---
+++ src/main/java/catalog/angularjs/dao/BookDao.java
@@ -0,0 +1,7 @@
+package catalog.angularjs.dao;
+
+/**
+ * Created by evgen on 13.08.15.
+ */
+public class BookDao {
+}
---
+++ src/main/java/catalog/angularjs/dao/impl/BookDaoImpl.java
@@ -0,0 +1,7 @@
+package catalog.angularjs.dao.impl;
+
+/**
+ * Created by evgen on 13.08.15.
+ */
+public class BookDaoImpl {
+}
